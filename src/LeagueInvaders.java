33d04042171e14709774eae036773c14f641db58
root <root@41277b05947f>
2021-01-04 21:30:47 +0000

    commit

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,21 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame; 
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel GP = new GamePanel();
+	LeagueInvaders(){
+		frame = new JFrame();
+	}
+	void setup() {
+		frame.add(GP);
+		frame.setVisible(true);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+public static void main(String[] args) {
+	LeagueInvaders LI = new LeagueInvaders();
+	LI.setup();
+}
 }
